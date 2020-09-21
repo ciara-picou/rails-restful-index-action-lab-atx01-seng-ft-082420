2cca918f8c45539f39486d643be0bf60e7f4afd2
ciara-picou <ciarapicou@att.net>
2020-09-21 12:03:06 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #resources :students, only: :index
+  # line 3 is a way to write multiple routes at once
+  # get'/route_name =>  'controller_name#controller_action aka method'
+  #get '/index' => 'students/#index'...wonky!
+  #corrected version:
+  get "/students" => "students#index"
+  #would line 6 also work?
 end
